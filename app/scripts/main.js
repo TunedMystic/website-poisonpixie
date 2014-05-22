0db0081b1d3fb7b7009b70bd16276e9c33fd4683
TunedMystic <jadoonanansandeep@gmail.com>
2014-05-22 02:10:52 -0400

    Site structure almost done. Working on fonts and colors after adding more dresses.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,8 +5,10 @@ $(document).ready(function() {
   var options = {
     columnWidth: 380,
     itemSelector: '.block-outer',
-    gutter: 10,
-    transitionDuration: '0.9s'
+    //gutter: 10,
+    gutter: 20,
+    //isAnimated: false,
+    transitionDuration: "0.5s"
   };
   
   $(".main-grid").masonry(options);
